8b1294d011f10746f13c657b3196a162f5888fc5
goh-joshua <joshuagoh321@gmail.com>
2025-08-21 21:07:29 +0800

    Modify program to echo user's input

--- src/main/java/Burh.java
+++ src/main/java/Burh.java
@@ -1,8 +1,35 @@
+import java.util.Scanner;
+
+
 public class Burh {
-    public static void main(String[] args) {
-        System.out.println("Hello! I'm Burh");
-        System.out.println("What can I do for you?");
+    /**
+     * Function to print lines
+     */
+    public static void printLines() {
         System.out.println("_-".repeat(20));
-        System.out.println("Burh...");
+    }
+
+    public static void main(String[] args) {
+        // Creates a scanner to detect user input in order to reply
+        Scanner scanner = new Scanner(System.in);
+        // To help detect when to end to program
+        boolean stop = false;
+
+        System.out.println("Bello! I'm Burh");
+        System.out.println("What can I do for you?");
+        printLines();
+
+        while (!stop) {
+            //Gets user input
+            String input = scanner.nextLine();
+
+            if (input.equals("bye")) {
+                stop = true;
+                System.out.println("Burh, goodbye!");
+            } else {
+                System.out.println(input);
+            }
+            printLines();
+        }
     }
 }
