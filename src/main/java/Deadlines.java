1ef530cca5368819ce33079e59a764ad39c07cae
goh-joshua <joshuagoh321@gmail.com>
2025-08-21 22:45:09 +0800

    Add Deadline and Event class

---
+++ src/main/java/Deadlines.java
@@ -0,0 +1,13 @@
+public class Deadlines extends Task{
+    private String dueDate;
+
+    public Deadlines(String task, String dueDate) {
+        super(task);
+        this.dueDate = dueDate;
+    }
+
+    @Override
+    public String toString() {
+        return "[D]" + super.toString() + " (by: " + dueDate +")";
+    }
+}
---
+++ src/main/java/Event.java
@@ -0,0 +1,15 @@
+public class Event extends Task {
+    private String To;
+    private String From;
+
+    public Event(String task, String From, String To) {
+        super(task);
+        this.To = To;
+        this.From = From;
+    }
+
+    @Override
+    public String toString() {
+        return "[E]" + super.toString() + " (from: " + From + " to: " + To + ")";
+    }
+}
