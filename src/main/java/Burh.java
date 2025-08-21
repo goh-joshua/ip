d0e6f1e4d512fd78dfe1afb3a01732e98a06194f
goh-joshua <joshuagoh321@gmail.com>
2025-08-21 20:28:58 +0800

    Change name and modify the greeting

---
+++ src/main/java/Burh.java
@@ -0,0 +1,8 @@
+public class Burh {
+    public static void main(String[] args) {
+        System.out.println("Hello! I'm Burh");
+        System.out.println("What can I do for you?");
+        System.out.println("_-".repeat(20));
+        System.out.println("Burh...");
+    }
+}
