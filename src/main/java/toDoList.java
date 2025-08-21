ce9c1d529c1974bd4b70a1c1951ce1ff5b192036
goh-joshua <joshuagoh321@gmail.com>
2025-08-21 21:41:02 +0800

    Create toDoList Class

---
+++ src/main/java/toDoList.java
@@ -0,0 +1,45 @@
+import java.util.ArrayList;
+
+public class toDoList {
+    private ArrayList<String> tasks = new ArrayList<>();
+    private ArrayList<Boolean> completed = new ArrayList<>();
+
+    // Initializer
+    public toDoList() {
+
+    }
+
+    public void addTask(String task) {
+        tasks.add(task);
+        completed.add(false);
+    }
+
+    public void completeTask(int i) {
+        if (i >= 1 && i < completed.size()) {
+            completed.set(i - 1, true);
+            System.out.println("Bokay, you have done this task 👍");
+            System.out.println("    [❌] " + tasks.get(i-1));
+        }
+    }
+
+    public void uncompleteTask(int i) {
+        if (i >= 1 && i < completed.size()) {
+            completed.set(i - 1, false);
+            System.out.println("Burh, seriously?");
+            System.out.println("    [  ] " + tasks.get(i-1));
+        }
+    }
+
+    public void orderedPrint() {
+        String done;
+        for (int i = 0; i < this.tasks.size(); i++) {
+            if (this.completed.get(i)) {
+                done = "❌";
+            } else {
+                done = "  ";
+            }
+            System.out.println((i + 1) + ". [" + done + "] " + this.tasks.get(i));
+        }
+    }
+
+}
